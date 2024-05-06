29baa9d59cfcf4271900bfd77e6a09d95a98e0c5
jitendra7518 <126078193+jitendra7518@users.noreply.github.com>
2024-05-06 11:45:57 +0530

    all activity api update

--- controller/adminController.js
+++ controller/adminController.js
@@ -6,14 +6,11 @@ const activityModel = require('../models/activityModel');
 
 exports.getAllMetaData = async (req, res) => {
     try {
-
-
         const pipeline = [{
             $group: {
                 _id: '$status',
                 count: { $sum: 1 }
             }
-
         }]
         const data = await Promise.all([
             userModel.aggregate([{
@@ -45,8 +42,43 @@ exports.getAllUsersList = async (req, res) => {
         }])
         res.status(200).send({ success: true, msg: "", data: users })
     }
+    catch (err) {
+        res.status(500).send({ success: false, msg: err.message, data: [] })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const { id } = req.query
+        const result = await userModel.findByIdAndDelete({ _id: id })
+        if (result) {
+            res.status(200).send({ success: true, msg: "user deleted successfully", data: [] })
+        }
+        else {
+            res.status(404).send({ success: false, msg: "user not found", data: [] })
+        }
+    }
+    catch (err) {
+        res.status(500).send({ success: false, msg: err.message, data: [] })
+    }
+
+
+}
+
+exports.updateUserDetails = async (req, res) => {
+    try{
+     const {id} = req.query
+     const UpdatedData = req.body
+     const result = await userModel.findByIdAndUpdate(id,UpdatedData,{new:true})
+     if(result){
+        res.status(200).send({sucess:true,msg:"",data:result})
+     }
+     else{
+        res.status(500).send({sucess:false,msg:"something went wrong",data:[]})
+     }
+    }
     catch(err){
-        res.status(500).send({ success: false, msg:err.message, data:[] })
+        res.status(500).send({sucess:false,msg:err.message,data:[]})
     }
 }
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const {getAllMetaData,getAllUsersList} = require('../controller/adminController');
+const {getAllMetaData,getAllUsersList,deleteUser,updateUserDetails} = require('../controller/adminController');
 
 router.get("/get_dashboard_data",getAllMetaData);
 router.get("/get_users_List",getAllUsersList);
-
+router.delete('/delete_user_by_Id',deleteUser)
+router.patch('/update_user_by_Id',updateUserDetails)
 
 module.exports = router
